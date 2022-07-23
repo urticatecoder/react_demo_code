2db1265015f6dee79a0d83fe3a1dc13453fb259a
Keaton Hawkins <keatonhhawkins@gmail.com>
2022-07-23 17:41:46 -0400

    complete commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,185 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Square(props) {
+    if (props.highlight) {
+        return (
+            <button className = "highlightedSpot" onClick = {props.onClick}>
+                {props.value}
+            </button>
+        )
+    }
+    
+    return (
+        <button className = "boardSpot" onClick = {props.onClick}>
+            {props.value}
+        </button>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square highlight = {this.props.highlightedSquares[i]} value = {this.props.values[i]} onClick = {() => this.props.onClick(i)} />
+    }
+
+    render() {
+        let board;
+        return (
+            <div id = "boardWrapper">
+                <div>
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div>
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div>
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+class Game extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            values: [
+                {squares: [null, null, null, null, null, null, null, null, null]},
+            ],
+            isXTurn: true,
+            currentlyDisplayedMove: 0,
+            moveTracker: new Map(),
+            reversedMovesList: false,
+            highlightedSquares: [false, false, false, false, false, false, false, false, false],
+        }
+    }
+
+    handleSquareClick(i) {
+        let history = this.state.values.slice(0, this.state.currentlyDisplayedMove + 1);
+        let currentBoard = history[history.length - 1].squares.slice();
+
+        if (currentBoard[i] || this.checkWinner(currentBoard)) {
+            return;
+        }
+
+        currentBoard[i] = this.state.isXTurn ? "X" : "O";
+        let moveTracker = new Map(this.state.moveTracker);
+        moveTracker.set(this.state.currentlyDisplayedMove + 1, (this.state.isXTurn ? "X" : "O") + i);
+        
+        this.setState({
+            values: history.concat([{
+                squares : currentBoard,
+            }]),
+            isXTurn: !this.state.isXTurn,
+            currentlyDisplayedMove: this.state.currentlyDisplayedMove + 1,
+            moveTracker: moveTracker,
+        })
+    }
+
+    checkWinner(board) {
+        let lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for (let i = 0; i < lines.length; i++) {
+            let [a, b, c] = lines[i];
+            if (board[a] && board[a] == board[b] && board[a] == board[c]) {
+                return board[a];
+            }
+        }
+    }
+
+    getButtons() {
+         let buttons = this.state.values.map(
+            (board, move) => {
+                let message;
+                if (move === 0) {
+                    message = "Go to game start"; 
+                } else {
+                    let movePosRaw = this.state.moveTracker.get(move);
+                    let movePosFormatted = movePosRaw[0] + " at (" + (Number(movePosRaw[1]) % 3) + ", " + (Math.floor(Number(movePosRaw[1]) / 3)) + ")"; 
+                    message = "Go to move " + move + ": " + movePosFormatted;
+                }
+
+                if (move === this.state.currentlyDisplayedMove) {
+                    return (
+                        <li key = {move}>
+                            <button onClick = {() => this.switchTo(move)}><b>{message}</b></button>
+                        </li>
+                    );
+                }
+
+                return (
+                    <li key = {move}>
+                        <button onClick = {() => this.switchTo(move)}>{message}</button>
+                    </li>
+                );
+            }
+        );
+
+        if (this.state.reversedMovesList) {
+            return buttons.reverse();
+        }
+
+        return buttons;
+    }
+
+    switchTo(move) {
+        this.setState({
+            currentlyDisplayedMove: move,
+            isXTurn: move % 2 === 0 ? true : false,
+        });
+    }
+
+    render() {
+        let history = this.state.values;
+        let currentBoard = history[this.state.currentlyDisplayedMove].squares.slice();
+        let timeTravelButtons = this.getButtons();
+        let statusMessage;
+
+        if(this.checkWinner(currentBoard)) {
+            statusMessage = "Congratulations " + this.checkWinner(currentBoard) + ", you win!"
+        } else {
+            statusMessage = "Next turn: " + (this.state.isXTurn ? "X" : "O");
+        }
+
+        return (
+            <div>
+                <Board 
+                    values = {currentBoard}
+                    onClick = {(i) => this.handleSquareClick(i)}
+                    highlightedSquares = {this.state.highlightedSquares}     
+                />
+                <div>{statusMessage}</div>
+                <div>
+                    <ul>
+                        {timeTravelButtons}
+                    </ul>
+                    <button id = "toggleMovesListButton" onClick = {() => this.setState({reversedMovesList: !this.state.reversedMovesList})}>Reverse moves list</button>
+                </div>
+            </div>
+            
+        )
+    }
+}
+
+/* ============================================== */
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
